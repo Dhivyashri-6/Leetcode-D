0100e12d8cefc21515932620d0dbe4b84df2916f
Dhivyashri k k <kkdhivyashri@gmail.com>
2026-02-14 22:25:59 +0530

    Time: 3 ms (91.01%), Space: 44.8 MB (57.46%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,9 +3,16 @@ class Solution {
         if(s.length()!=t.length())return false;
         char[] a=s.toCharArray();
         char[] b=t.toCharArray();
-        
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+        int[] count=new int[26];
+       for(char c:a){
+        count[c-'a']++;
+       }
+           for(char c:b){
+        count[c-'a']--;
+       }
+       for(int val:count){
+        if(val!=0)return false;
+       }
+       return true;
     }
 }
